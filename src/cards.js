146afebd6c3e7dc068902ed71c13bd82a5be7c2f
Seun1601 <114428457+Seun1601@users.noreply.github.com>
2023-02-06 22:48:55 +0100

    God abeg

--- src/cards.js
+++ src/cards.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const cards = (props) => {
   return (
-    <div className="card-gangan text-white flex flex-col justify-between items-center w-1/6 bg-green-500 ">  
+    <div className="card-gangan text-white flex flex-col justify-between items-center w-32 h-32">  
 
-        <div className="bigbox bg-black w-full h-full flex flex-col ">
-            <div className="smallbox1  bg-purple-800 w-full h-1/2">
-                
+        <div className="bigbox bg-black w-full h-full flex flex-col relative">
+            <div className="smallbox1   w-full h-1/2 z-auto ">
+            
             </div>
-            <div className="smallbox2 bg-purple-400 w-full h-1/2">
+            <div className="smallbox2  w-full h-1/2 z-auto bg-lime-500">
         
             </div>
-        </div>
-         <div className="number .z-20">
+         <div className="number z-20 absolute top-1/2 " >
           {props.time}
          </div>
+        </div>
 
         <div className="countBy .justify-self-baseline">
             <h2>{props.countBy}</h2>
